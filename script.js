bfd2afbc0a9502fa79f3a9dd39f1c5ca00e62fc1
alxcyha <aliciamargaret2192@gmail.com>
2023-05-07 19:01:40 +0800

    updated design

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-let display = document.getElementById('screen');
+let display = document.getElementById("screen");
 
-const wipe = () => {
-    display.value = '';
+function wipe(){
+    display.value = "";
 }
-const del = () => {
+function del(){
     display.value = display.value.slice(0, -1);
 }
-const show = (n) =>{
+function show(n){
     display.value += n;
 }
 
-const calc = () => {
-    display.value = eval(display.value);
+function calc(){
+    try{display.value = eval(display.value);}
+    catch(error){alert("Invalid, try again.")}
 }
