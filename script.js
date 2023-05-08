64c0e609aff50d825709ed9b8b4550d12a86bfca
alxcyha <aliciamargaret2192@gmail.com>
2023-05-08 20:37:22 +0800

    simple calculator

--- script.js
+++ script.js
@@ -3,14 +3,31 @@ let display = document.getElementById("screen");
 function wipe(){
     display.value = "";
 }
+
 function del(){
     display.value = display.value.slice(0, -1);
 }
+
 function show(n){
     display.value += n;
 }
 
-function calc(){
-    try{display.value = eval(display.value);}
-    catch(error){alert("Invalid, try again.")}
+function percentage() {
+	let result = 0;
+	result = Number.parseFloat(display.value) / 100;
+    document.getElementById('screen').value = result;
 }
+
+function calc(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        alert("Invalid, try again.");
+    }
+}
+
+
+
+
+
